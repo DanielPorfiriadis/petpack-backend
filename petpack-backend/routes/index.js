f16a89f225c0e5d50429c7d32be3a048fe900296
naya <panagiota.p@skgcode.gr>
2020-10-15 21:06:22 +0300

    changes for hash

--- petpack-backend/routes/index.js
+++ petpack-backend/routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const router = express.Router();
 const UserData = mongoose.model('UserData');
 const PetData = mongoose.model('PetData');
-
+var BCRYPT_SALT_ROUNDS = 4;
 router.get('/', (req, res) => {
     res.send('test  slash ok');
 });
@@ -27,7 +28,19 @@ router.post('/register', (req, res) => {
             userData.userName = req.body.userName;
             userData.firstName = req.body.firstName;
             userData.lastName = req.body.fastName;
-            userData.password = req.body.password;
+            var password = req.body.password;
+            bcrypt.hash(password, BCRYPT_SALT_ROUNDS)
+                .then(function (hashedPassword) {
+                    //res.send('Hash ok');
+                    console.log(hashedPassword);
+                    password = hashedPassword;
+                })
+
+                .catch(function (error) {
+                    console.log("Error hashing ");
+                    console.log(error);
+                });
+            userData.password = password;
             userData.email = req.body.email;
             userData.birthDate = req.body.birthDate;
             petData.petName = req.body.petName;
