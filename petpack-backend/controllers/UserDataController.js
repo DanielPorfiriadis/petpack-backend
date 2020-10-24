4f26d8664f14333332de000f2c6705a263f3e69e
Daniel <danisporfiriadis@gmail.com>
2020-10-24 22:23:35 +0300

    minor login API Changes to avoid crushing

--- petpack-backend/controllers/UserDataController.js
+++ petpack-backend/controllers/UserDataController.js
@@ -2,41 +2,48 @@ const mongoose = require('mongoose');
 const UserData = mongoose.model('UserData');
 const bcrypt = require('bcrypt');
 const userData = new UserData();
+
 exports.login_user = function (req, res) {
+
+
     const givenUsername = req.body.userName;
     const givenPassword = req.body.password;
 
     //Retrieve User data from database
-    UserData.findOne({ userName: givenUsername }, function (err, userData) { });
-/*    if (err) {
-        //TODO: Here we need to implement appropiate error respond
-        res.send("login error");
-    }*/
-/*    if (!userData) {
-        //TODO: Here we need to implement appropiate error respond
-        res.send("incorrect username!");
-    }*/
-    //check if the hashed password if same with given password
-    bcrypt.compare(givenPassword, userData.password, function (err, isMatch) {
+    UserData.findOne({ userName: givenUsername }, function (err, userData) { 
         if (err) {
             //TODO: Here we need to implement appropiate error respond
             res.send("login error");
-        } else if (!isMatch) {
+        }
+        if (!userData) {
             //TODO: Here we need to implement appropiate error respond
-            res.send("Password doesn't match!");
-        } else {
-            //TODO: Here we need to implement appropiate error respond
-            res.send("Password matches!");
+            res.send("incorrect username!");
+        }
+        else {
+            //check if the hashed password if same with given password
+            bcrypt.compare(givenPassword, userData.password, function (bcryptErr, isMatch) {
+                if (bcryptErr) {
+                    //TODO: Here we need to implement appropiate error respond
+                    res.send("login error");
+                } else if (!isMatch) {
+                    //TODO: Here we need to implement appropiate error respond
+                    res.send("Password doesn't match!");
+                } else {
+                    //TODO: Here we need to implement appropiate error respond
+                    res.send("Password matches!");
+                }
+            });
         }
     });
+
 }
 
 exports.register_user = function (req, res) {
    
     //check if user credentials are already in database
     //TODO: We have to check if user's email exist in database
-    UserData.exists({ name: req.body.userName }, function (err, exists) {
-        if (exists == false) {
+    UserData.exists({ userName: req.body.userName }, function (err, exists) {
+        if (exists == false ) {
             userData.userName = req.body.userName;
             userData.firstName = req.body.firstName;
             userData.lastName = req.body.lastName;
