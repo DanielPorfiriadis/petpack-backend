f9b02464778858fd8d4364dcba0a618af4117588
Daniel <danisporfiriadis@gmail.com>
2020-10-30 15:34:14 +0200

    Added bodyParser correctly

--- petpack-backend/app.js
+++ petpack-backend/app.js
@@ -8,6 +8,7 @@ const app = express();
 
 app.use(cookieParser())
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 //Below are the route classes we want to run
 app.use('/', routesIndex);
